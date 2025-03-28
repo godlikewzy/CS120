24a798fbd16eff44f27f9bb38a3d4b3ec2beca5a
Leo <58340663+godlikewzy@users.noreply.github.com>
2025-03-27 23:06:00 -0400

    Add files via upload

---
+++ fibo.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<title>problem 1</title>
+</head>
+
+<body>
+<?php
+// header('Content-Type: application/json');
+
+if (is_numeric($_GET['n'])) {
+    $n = (int) $_GET['n'];
+    
+    $fibo = [];
+    if ($n >= 1) $fibo[] = 0;
+    if ($n >= 2) $fibo[] = 1;
+    for ($i = 2; $i < $n; $i++) {
+        $fibo[] = $fibo[$i - 1] + $fibo[$i - 2];
+    }
+
+    $result = [
+        'length' => $n,
+        'fibSequence' => $fibo
+    ];
+} 
+else {
+    $result = [
+        'error' => "Missing or invalid 'n' parameter."
+    ];
+}
+
+echo json_encode($result);
+?>
+</body>
+</html>
